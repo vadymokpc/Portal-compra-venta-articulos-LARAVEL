2dc617e2e413b7277edd426d0656ace60ac85cbf
Vadym Ostapchuk <vadymostapchuk@Vadyms-MacBook-Air.local>
2021-09-16 16:34:58 +0200

    3 completada

--- app/Http/Controllers/PublicController.php
+++ app/Http/Controllers/PublicController.php
@@ -1,34 +1,37 @@
-<?php
-
-namespace App\Http\Controllers;
+<?php namespace App\Http\Controllers;
 
 use App\Models\Ad;
 use App\Models\Category;
 use Illuminate\Http\Request;
 
-class PublicController extends Controller
-{
-    public function index()
-    {
-/*  -- --------------------------------Visualizacion ultimos 6 anuncios------------------------------ -- */
-      $ads = Ad::orderBy('created_at','desc')->take(6)->get();
+class PublicController extends Controller {
+    public function index() {
+        /*--------------------------------------Flitrado de anuncios aceptados por revisor-----------------------------------------------------*/
+        $ads = Ad::where('is_accepted',true)
+        ->orderBy('created_at', 'desc') 
+        ->take(6)                         /* Visualizacion ultimos 6 anuncios en orden descendiente */
+        ->get();
+        return view('home', compact('ads'));
+        /*---------------------------------------Flitrado de anuncios aceptados por revisor----------------------------------------------------*/
+    }
 
-      return view('home',compact('ads'));
-/*  -- --------------------------------Visualizacion ultimos 6 anuncios--------------------------------------------- */
+    /*  -- --------------------------------?????????Visualizar anuncios agrupados por categorias------------------------------ -- */
+    public function adsByCategory($name, $category_id) {
+
+        $category=Category::find($category_id);
+        $ads=$category->ads() ->where('is_accepted', true) ->orderBy('created_at', 'desc') ->paginate(5);
+        return view('ads', compact('category', 'ads'));
     }
- /*  -- --------------------------------Visualizar anuncios agrupados por categorias------------------------------ -- */  
-public function adsByCategory($name, $category_id){
-    $category = Category::find($category_id);     
-    $ads = $category->ads()->paginate(5);
-   
-    return view('ads', compact ('category', 'ads'));
-}
-/*  -- --------------------------------Visualizar anuncios agrupados por categorias------------------------------ -- */ 
-/*--------------------------------------Pagina detalle de cada anuncio-----------------------------------------------------*/
-public function details($id) 
-    {
-        $ad = Ad::findOrFail($id);
-        return view("ad.details",["ad"=>$ad]);
+
+    /*  -- --------------------------------????????????Visualizar anuncios agrupados por categorias------------------------------ -- */
+    /*--------------------------------------Pagina detalle de cada anuncio-----------------------------------------------------*/
+    public function details($id) {
+        $ad=Ad::findOrFail($id);
+        return view("ad.details", ["ad"=>$ad]);
     }
-/*--------------------------------------Pagina detalle de cada anuncio-----------------------------------------------------*/
+
+    /*--------------------------------------Pagina detalle de cada anuncio-----------------------------------------------------*/
+
+
+
 }
