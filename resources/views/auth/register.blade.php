9088a9c43983c63bac85b4f251641b609cd3d3ff
Vadym Ostapchuk <vadymostapchuk@Vadyms-MacBook-Air.local>
2021-09-19 13:33:18 +0200

    navbar refactorizada

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,11 +1,10 @@
-
-@extends('layouts.app')  
-@section('content') 
+@extends('layouts.app')
+@section('content')
 <!-- ======= REGISTER ======= -->
 <div class="container-fluid bg-accent vh-100">
-       <div class="row mb-5 pb-5">
-            <div class="col-12 col-md-8 ms-5">
-                <div class="d-flex flex-column align-items-center ">
+    <div class="row mb-5 pb-5">
+        <div class="col-12 col-md-8 ms-5">
+            <div class="d-flex flex-column align-items-center ">
                 <div class="form-content justify-content-center mb-5 pb-5">
                     <!--FORM TITLE -->
                     <div class="section-title">
@@ -28,8 +27,9 @@
                         @csrf
                         <!--Name -->
                         <div class="form-field-edit form-field space-around my-2">
-                          <input type="text" name="name" id="name" class="form-control forms_field-input" placeholder="Your Name" data-rule="minlen:4" data-msg="Please enter at least 4 chars">
-                          <div class="validate"></div>
+                            <input type="text" name="name" id="name" class="form-control forms_field-input"
+                                placeholder="Your Name" data-rule="minlen:4" data-msg="Please enter at least 4 chars">
+                            <div class="validate"></div>
                         </div>
                         <!--Email -->
                         <div class="form-field-edit form-field space-around my-2">
@@ -43,10 +43,10 @@
                                 placeholder="Your Password">
                             <div class="validate"></div>
                         </div>
-                         <!--Password Confirmation -->
-                         <div class="form-field-edit form-field space-around my-2">
-                            <input type="password" name="password_confirmation" id="password" class="form-control forms_field-input"
-                                placeholder="Your Password">
+                        <!--Password Confirmation -->
+                        <div class="form-field-edit form-field space-around my-2">
+                            <input type="password" name="password_confirmation" id="password"
+                                class="form-control forms_field-input" placeholder="Your Password">
                             <div class="validate"></div>
                         </div>
                         <!--Button-Register-->
@@ -56,14 +56,12 @@
                     </form>
                 </div>
                 <div class="form-link mt-4 d-flex">
-                <p class="text-white">You dont have an account</p>
-                <a class="text-reset text-decoration-none ps-2" href="{{route('login')}}"><u>Login here</u></a>
+                    <p class="text-white">You dont have an account</p>
+                    <a class="text-reset text-decoration-none ps-2" href="{{route('login')}}"><u>Login here</u></a>
                 </div>
             </div>
-            <div class="col-12 col-md-4">
-               <p>lorem</p>
-            </div>
+
         </div>
     </div>
-</section>
-@endsection
+    </section>
+    @endsection
