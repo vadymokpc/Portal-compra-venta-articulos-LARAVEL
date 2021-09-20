571c071e9797cbcea4a207990a2ad0043e530adf
Vadym Ostapchuk <vadymostapchuk@Vadyms-MacBook-Air.local>
2021-09-20 20:11:53 +0200

    boton login arreglado3

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -6,8 +6,8 @@
         <div class="col-12 d-flex flex-column justify-content-center align-items-center">
             <div class="d-flex flex-column justify-content-center align-items-center">
                 <!--FORM TITLE -->
-                    <h2 class="">LOGIN</h2>
-                
+                <h2 class="">LOGIN</h2>
+
                 @if ($errors->any())
                 <div class="alert alert-danger">
                     <ul>
@@ -18,28 +18,38 @@
                 </div>
                 @endif
                 <!--FORM FIELDS -->
-                <form action="/login" method="POST" role="form" class="d-flex flex-column justify-content-center align-items-center">
+                <form action="/login" method="POST" role="form"
+                    class="d-flex flex-column justify-content-center align-items-center">
                     @csrf
                     <!--Email -->
-                    
-                        <input type="email" name="email" id="email" class="form-control forms_field-input"
-                            placeholder="Your Email" data-rule="minlen:4" data-msg="Please enter at least 4 chars">
-                        
-                    
+
+                    <input type="email" name="email" id="email" class="form-control forms_field-input"
+                        placeholder="Your Email" data-rule="minlen:4" data-msg="Please enter at least 4 chars">
+
+
                     <!--Password -->
-                    
-                        <input type="password" name="password" id="password" class="form-control forms_field-input mt-2"
-                            placeholder="Your Password">
-                        
+
+                    <input type="password" name="password" id="password" class="form-control forms_field-input mt-2"
+                        placeholder="Your Password">
+
                     <!--Button-Login-->
-                    <button type="submit" class=" text-center my-2">
-                        Log-In
-                    </button>
+                    <div class="align-items-center my-2">
+                        <button href="#">
+                            <span></span>
+                            <span></span>
+                            <span></span>
+                            <span></span>
+                            Log-in
+                        </button>
+                    </div>
+                    <!--Button-Login-->
+
                 </form>
             </div>
             <div class="form-link d-flex flex-column justify-content-center align-items-center ">
                 <p class="text-white mb-0">¿No estás registrado? </p>
-                <a class="text-reset text-decoration-none mt-0 ms-2 " href="{{route('register')}}"><u> ¡Registrate aqui!</u></a>
+                <a class="text-reset text-decoration-none mt-0 ms-2 " href="{{route('register')}}"><u> ¡Registrate
+                        aqui!</u></a>
             </div>
         </div>
     </div>
