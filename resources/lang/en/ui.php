e41191ff74bf7908c554bf39437bfe1f43336a88
AlexPala <alexapalacios9119@gmail.com>
2021-09-23 17:55:25 +0200

    modifico home

--- resources/lang/en/ui.php
+++ resources/lang/en/ui.php
@@ -4,9 +4,12 @@ return [
     'welcome'=>'Welcome to rapido.es',
     'New Ad'=>'New Ad',
     'categories'=>'Categories',
+    'categorie'=>'Categorie',
     'register'=>'Register',
     'createdby'=>'Created by',
     'checkrevisor'=>'There are no ads to review!',
     'login'=>'Login',
+    'logout'=>'logout',
+    'segundoh1'=>'Sell ​​what you no longer use!'
 ];
 /* -----------------------------------------Middleware para configurar el idioma en session-------------------------------------------------- */
--- resources/lang/it/ui.php
+++ resources/lang/it/ui.php
@@ -4,9 +4,12 @@ return [
     'welcome'=>'Benvenuto su rapido.es',
     'New Ad'=>'Nuovo Annuncio',
     'categories'=>'Categorie',
+    'categorie'=>'Categorie',
     'register'=>'Registrati',
     'createdby'=>'Creato da',
     'checkrevisor'=>'Non ci sono annunci da recensire!',
     'login'=>'Accesso',
+    'logout'=>'Disconnettersi',
+    'segundoh1'=>'Vendi quello che non usi più!',
 ];
 /* -----------------------------------------Middleware para configurar el idioma en session-------------------------------------------------- */
